02080db260e60d2c26cf44d1a28b8e813e3ae94c
Mitchell <mwong@deerfield.edu>
2016-02-01 00:08:32 -0500

    Made significant changes on what is now the final product

--- src/ColorChooser.java
+++ src/ColorChooser.java
@@ -1,30 +1,96 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JColorChooser;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.accessibility.AccessibleContext;
+import javax.accessibility.AccessibleState;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+import javax.swing.plaf.basic.ComboPopup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.plaf.ComboBoxUI;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicArrowButton;
+import javax.swing.plaf.basic.BasicComboBoxUI;
 
-public class ColorChooser {
-  public static void main(String args[]) {
-    JFrame f = new JFrame("JColorChooser Sample");
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    final JButton button = new JButton("Pick to Change Background");
 
-    ActionListener actionListener = new ActionListener() {
-      public void actionPerformed(ActionEvent actionEvent) {
-        Color initialBackground = button.getBackground();
-        Color background = JColorChooser.showDialog(null, "JColorChooser Sample", initialBackground);
-        if (background != null) {
-          button.setBackground(background);
-        }
-      }
-    };
-    button.addActionListener(actionListener);
-    f.add(button, BorderLayout.CENTER);
-    f.setSize(300, 200);
-    f.setVisible(true);
-  }
+public class ColorChooser extends JComboBox {
+	public static void main(String[] args) {
+	
+		  String output = "<html>"
+	  			+ "The quick brown fox jumped over the lazy dog’s back.<br>"
+	  			+ "Pack my box with five dozen liquor jugs.<br>"
+	  			+ "Jackdaws love my big sphinx of quartz.<br>"
+	  			+ "Mr. Jock, TV quiz PhD, bags few lynx.<br>"
+	  			+ "abcdefghijklmnopqrstuvwxyz<br>"
+	  			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>"
+	  			+ "01234567890<br>"
+	  			+ "€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥<br>"
+	  			+ "ÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ<br>"
+	  			+ "àáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ<br>"
+	  			+ "!\"#$%&\'()*+,-./:;<=>?@[\\^_z{|}~<br>"
+	  			+ "uvw wW gq9 2z 5s il17|!j oO08 `'\" ;:,. m nn rn {[()]}u<br>"
+	  			+ "</html>";
+	  	  JLabel example = new JLabel (output,JLabel.CENTER);
+		
+          GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	      String[] names = ge.getAvailableFontFamilyNames();
+	      JList fontstyles = new JList(names);
+	      JScrollPane fonts = new JScrollPane(fontstyles);
+	      class FontCellRenderer extends DefaultListCellRenderer {
+	    	    public Component getListCellRendererComponent(
+	    	        JList list,
+	    	        Object value,
+	    	        int index,
+	    	        boolean isSelected,
+	    	        boolean cellHasFocus) {
+	    	        JLabel labela = (JLabel)super.getListCellRendererComponent(
+	    	            list,value,index,isSelected,cellHasFocus);
+	    	        Font font = new Font((String)value, Font.PLAIN, 16);
+	    	        labela.setFont(font);
+	    	        return labela;
+	    	    }
+	    	}
+	      fontstyles.setCellRenderer(new FontCellRenderer());
+	      JLabel labelb = new JLabel("Font families available:");
+	      JLabel labelc = new JLabel("Select a style:");
+	      JButton button1 = new JButton ("Text Color");
+	      JButton button2 = new JButton ("Background Color");
+	      
+	      JPanel panel_one = new JPanel();
+	      panel_one.setLayout(new GridLayout(0,1));
+	      panel_one.setSize(new Dimension(300, 300));
+	      
+	      JPanel panel_two = new JPanel();
+	      panel_two.setSize(new Dimension(300,300));
+	      
+	      JTabbedPane panel_three = new JTabbedPane();
+	      JColorChooser fore = new JColorChooser();
+	      JColorChooser back = new JColorChooser();
+	      panel_three.addTab("Font Styles", panel_two);
+	      panel_three.addTab("Text Color", fore);
+	      panel_three.addTab("Background Color", back);
+	      
+	      JFrame preview = new JFrame("Preview");
+	      preview.setLayout(new FlowLayout());
+	      preview.add(panel_three);
+	      preview.add(example);
+	      preview.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      preview.setSize(900, 500);
+	      preview.setVisible(true);
+	      panel_two.add(labelb);
+	      //family code
+	      
+	      panel_two.add(labelc);
+	      panel_two.add(fonts);
+	      panel_two.add(panel_one);
+
+	}
 }
